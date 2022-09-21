0e97445be709dc98f17ff6678cc9d725c1124969
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-21 23:03:19 +0530

    Update streamlit_app.py
    Fixed type in S3 url

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as slt
 import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/furit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 slt.title('My New Healthy Dinner')
 slt.header('Breakfast Favorites')
 slt.text('🍄 Omega 3 & Blueberry Oatmeal')
