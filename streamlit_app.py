3123c03426f73047a2eb2170d3d9eed43227eddd
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-21 22:56:12 +0530

    Update streamlit_app.py
    Added Emojies to the menu items
    Added another header for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 
 import streamlit as slt
 slt.title('My New Healthy Dinner')
-slt.header('Breakfast Menu')
-slt.text('Omega 3 & Blueberry Oatmeal')
-slt.text('Kale, Spinach & Rocket Smoothie')
-slt.text('Hard-Boiled Free-Range Egg')
+slt.header('Breakfast Favorites')
+slt.text('🍄 Omega 3 & Blueberry Oatmeal')
+slt.text('🌿 Kale, Spinach & Rocket Smoothie')
+slt.text('🐓 Hard-Boiled Free-Range Egg')
+slt.header('🍌🍇 Build your own smoothie 🍉🍋')
