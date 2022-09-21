5aa58be9d11d352704fc9722940978ecfa68edd5
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-21 23:01:52 +0530

    Update streamlit_app.py
    added line to read fruit_macro.csv file from AWS s3
    added line to load frutit list data into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 
 import streamlit as slt
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/furit_macros.txt")
 slt.title('My New Healthy Dinner')
 slt.header('Breakfast Favorites')
 slt.text('🍄 Omega 3 & Blueberry Oatmeal')
 slt.text('🌿 Kale, Spinach & Rocket Smoothie')
 slt.text('🐓 Hard-Boiled Free-Range Egg')
 slt.header('🍌🍇 Build your own smoothie 🍉🍋')
+slt.dataframe(my_fruit_list)
+
