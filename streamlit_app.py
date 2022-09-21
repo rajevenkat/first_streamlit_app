1b8bd1f8ead3d20a34a667cd008609b53826566f
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-21 22:47:46 +0530

    Update streamlit_app.py
    Added line for breakfast menu header
    Added lines for menu items under breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit as slt
-slt.title('My new healthy Dinner')
+slt.title('My New Healthy Dinner')
+slt.header('Breakfast Menu')
+slt.text('Omega 3 & Blueberry Oatmeal')
+slt.text('Kale, Spinach & Rocket Smoothie')
+slt.text('Hard-Boiled Free-Range Egg')
