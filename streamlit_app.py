dd1080e822c6ab7ec9b8f5bc634c9492f2a9762b
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-22 18:59:20 +0530

    Update streamlit_app.py
    fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as slt
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_frit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 slt.title('My New Healthy Dinner')
 slt.header('Breakfast Favorites')
 slt.text('🍄 Omega 3 & Blueberry Oatmeal')
