0b51f59bc3e4b556cdc11c2ad904479989231d9a
Rajesh Singamsetty <rajevenkat@gmail.com>
2022-09-22 18:58:22 +0530

    Update streamlit_app.py
    Added line to set index  and picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,13 @@ import streamlit as slt
 import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_frit_list = my_fruit_list.set_index('Fruit')
 slt.title('My New Healthy Dinner')
 slt.header('Breakfast Favorites')
 slt.text('🍄 Omega 3 & Blueberry Oatmeal')
 slt.text('🌿 Kale, Spinach & Rocket Smoothie')
 slt.text('🐓 Hard-Boiled Free-Range Egg')
 slt.header('🍌🍇 Build your own smoothie 🍉🍋')
+slt.multiselect("Pick some fruits:", list(my_fruit_list.index))
 slt.dataframe(my_fruit_list)
 
